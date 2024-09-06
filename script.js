4d40b4cfaf9ab651d8f8a2fa065fc12932ea9069
Carlos Augusto <gutoshistorias@gmail.com>
2024-09-06 18:06:58 -0300

    Consegui fazer o Filtro
    Fiz um filtro para trazer apenas das empresas que eu queria

--- script.js
+++ script.js
@@ -26,3 +26,50 @@ document.addEventListener("click", (event) =>{
         sidebar.style.left = "-200px";
     }
 })
+
+
+// Seleciona o botão "Limpar Tudo" (ajuste o seletor conforme necessário)
+const clearButton = document.querySelector('.filters a[href="#"]');
+
+// Adiciona um event listener ao botão
+clearButton.addEventListener('click', () => {
+  // Seleciona todos os checkboxes dentro da seção de filtros
+  const checkboxes = document.querySelectorAll('.filters input[type="checkbox"]');
+
+  // Desmarca todos os checkboxes
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+  });
+});
+
+
+const searchInput = document.getElementById('searchInput');
+const jobCards = document.querySelectorAll('.card');
+
+searchInput.addEventListener('input', (event) => {
+    const searchTerm = event.target.value.trim().toLowerCase();
+
+    jobCards.forEach(card => {
+        card.classList.remove('card-selected');
+
+        if (searchTerm) {
+            const jobTitle = card.querySelector('.job-title').innerText.toLowerCase();
+            const jobDescription = card.querySelector('.card-desc').innerText.toLowerCase();
+            let jobTagsText = '';
+            card.querySelectorAll('.card-tags a').forEach(tag => {
+                jobTagsText += tag.textContent.toLowerCase() + ' ';
+            });
+            const allText = `${jobTitle} ${jobDescription} ${jobTagsText}`;
+
+            const isMatch = allText.includes(searchTerm);
+
+            if (isMatch) {
+                card.classList.add('card-selected');
+            } else {
+                card.style.display = 'none';
+            }
+        } else {
+            card.style.display = 'block';
+        }
+    });
+});
